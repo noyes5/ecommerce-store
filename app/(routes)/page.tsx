9d9fa2d55bf30f8ae0c6a1c167b35c2c2c16f9a6
noyes5 <noyes5@naver.com>
2024-05-18 03:13:40 +0900

    chore: vercel 배포를 위한 repository 분리

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("c6553dcc-d150-4fc0-b4be-4a7f4f215447");
+  
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="상품 목록" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ components/ui/no-results.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return ( 
+    <div className="flex items-center justifyt-center h-full w-full text-neutral-500">
+      상품이 존재하지 않습니다.
+    </div>
+   );
+}
+ 
+export default NoResults;
